36c4fe01eb34b7c46bed92b3c78589d50a76cb96
JoseLuisRC18 <luis_0032@live.com.mx>
2025-02-25 19:50:16 -0600

    apx fin

--- ASO-PRUEBATEC/apx-cpnt-pruebatec/artifact/libraries/WIKJR888IMPL/src/main/java/com/bbva/wikj/lib/r888/impl/WIKJR888Impl.java
+++ ASO-PRUEBATEC/apx-cpnt-pruebatec/artifact/libraries/WIKJR888IMPL/src/main/java/com/bbva/wikj/lib/r888/impl/WIKJR888Impl.java
@@ -1,20 +1,39 @@
 package com.bbva.wikj.lib.r888.impl;
 
+import com.bbva.wikj.dto.user.DtoIn;
+import com.bbva.wikj.dto.user.DtoOut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * The WIKJR888Impl class...
- */
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class WIKJR888Impl extends WIKJR888Abstract {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(WIKJR888Impl.class);
 
-	/**
-	 * The execute method...
-	 */
+
 	@Override
-	public void execute() {
-		// TODO - Implementation of business logic
+	public DtoOut execute(DtoIn dtoIn) {
+		DtoOut dtoOut = new DtoOut();
+		Map<String, Object> args = new HashMap<>();
+		args.put("id",dtoIn.getId());
+		args.put("nuip",dtoIn.getNuip());
+		args.put("full_name",dtoIn.getFull_name());
+		args.put("phone",dtoIn.getPhone());
+		args.put("address",dtoIn.getAddress());
+
+		int result = this.wikjR889.executeInsert(args);
+		if(result == 1){
+			Map<String ,Object> response =  this.wikjR889.executeSelect(dtoIn.getId());
+
+			dtoOut.setId(response.get("id").toString());
+			dtoOut.setNuip(response.get("nuip").toString());
+			dtoOut.setFull_name(response.get("full_name").toString());
+			dtoOut.setPhone(response.get("phone").toString());
+			dtoOut.setAddress(response.get("address").toString());
+		}
+		return dtoOut;
 	}
 }
--- ASO-PRUEBATEC/apx-cpnt-pruebatec/artifact/transactions/WIKJT888-01-MX/src/main/java/com/bbva/wikj/WIKJT88801MXTransaction.java
+++ ASO-PRUEBATEC/apx-cpnt-pruebatec/artifact/transactions/WIKJT888-01-MX/src/main/java/com/bbva/wikj/WIKJT88801MXTransaction.java
@@ -1,5 +1,6 @@
 package com.bbva.wikj;
 
+import com.bbva.wikj.dto.user.DtoOut;
 import com.bbva.wikj.lib.r888.WIKJR888;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,8 @@ public class WIKJT88801MXTransaction extends AbstractWIKJT88801MXTransaction {
 	@Override
 	public void execute() {
 		WIKJR888 wikjR888 = this.getServiceLibrary(WIKJR888.class);
-		wikjR888.execute();
+		DtoOut dtoOut = wikjR888.execute(this.getDtoin());
+		this.setDtoout(dtoOut);
 	}
 
 }
